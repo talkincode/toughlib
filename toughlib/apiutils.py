cd9126efe281ada26b9e849f9f15dd993daaa320
jamiesun <jamiesun.net@gmail.com>
2015-12-30 15:57:57 +0800

    rename secret

--- toughlib/apiutils.py
+++ toughlib/apiutils.py
@@ -6,7 +6,7 @@ from hashlib import md5
 from toughlib import utils, httpclient
 
 
-def make_sign(secret, params=[]):
+def make_sign(api_secret, params=[]):
     """
         >>> make_sign("123456",[1,'2',u'中文'])
         '33C9065427EECA3490C5642C99165145'
@@ -19,7 +19,7 @@ def make_sign(secret, params=[]):
     return mds.upper()
 
 
-def check_sign(secret, msg):
+def check_sign(api_secret, msg):
     """
         >>> check_sign("123456",dict(code=1,s='2',msg=u'中文',sign='33C9065427EECA3490C5642C99165145'))
         True
@@ -29,31 +29,31 @@ def check_sign(secret, msg):
         return False
     sign = msg['sign']
     params = [utils.safestr(msg[k]) for k in msg if k != 'sign']
-    local_sign = make_sign(secret, params)
+    local_sign = make_sign(api_secret, params)
     return sign == local_sign
 
-def make_message(secret, **params):
+def make_message(api_secret, **params):
     """
         >>> json.loads(make_request("123456",**dict(code=1,msg=u"中文",nonce=1451122677)))['sign']
         u'58BAF40309BC1DC51D2E2DC43ECCC1A1'
     """
     if 'nonce' not in params:
         params['nonce' ] = str(int(time.time()))
-    params['sign'] = make_sign(secret, params.values())
+    params['sign'] = make_sign(api_secret, params.values())
     return json.dumps(params, ensure_ascii=False)
 
-def make_error(secret, msg=None):
+def make_error(api_secret, msg=None):
     """
         >>> json.loads(make_error("123456",msg="error")['sign']
         u'58BAF40309BC1DC51D2E2DC43ECCC1A1'
     """
     params = dict(code=1, msg=msg)
     params['nonce' ] = str(int(time.time()))
-    params['sign'] = make_sign(secret, params.values())
+    params['sign'] = make_sign(api_secret, params.values())
     return json.dumps(params, ensure_ascii=False)
 
 
-def parse_request(secret, reqbody):
+def parse_request(api_secret, reqbody):
     """
         >>> parse_request("123456",'{"nonce": 1451122677, "msg": "helllo", "code": 0, "sign": "DB30F4D1112C20DFA736F65458F89C64"}')
         {u'nonce': 1451122677, u'msg': u'helllo', u'code': 0, u'sign': u'DB30F4D1112C20DFA736F65458F89C64'}
@@ -63,7 +63,7 @@ def parse_request(secret, reqbody):
     except Exception as err:
         raise ValueError(u"parse params error")
 
-    if not check_sign(secret, req_msg):
+    if not check_sign(api_secret, req_msg):
         raise ValueError(u"message sign error")
 
     return req_msg
