05ddf2a535cebebbbdc557c3b44e3c32f4d80fac
jamiesun <jamiesun.net@gmail.com>
2016-04-12 12:05:15 +0800

    0.1.0

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,20 @@
 import sys,os,time
 sys.path.insert(0,os.path.dirname(__file__))
 from fabric.api import *
+from toughlib import __version__
+
+
+env.user = 'root'
+env.hosts = ['121.201.15.99']
+
+def pub():
+    try:
+        local("git add . && git ci -am '%s' && git push origin master"%raw_input("commit"))
+    except:
+        pass
+    with cd("/opt/toughlib"):
+        run("git pull origin master")
+        run("make upload")
 
 def test():
     local("pypy coverage run trial toughlib.tests")
