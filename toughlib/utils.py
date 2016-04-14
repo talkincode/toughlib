f2852cf8ae036b8e9e653f776570bf3fea82218a
tianxingxc@163.com <yangyi900116>
2016-04-14 17:00:37 +0800

    打印一下env

--- toughlib/utils.py
+++ toughlib/utils.py
@@ -38,8 +38,7 @@ class AESCipher:
         if key:self.setup(key)
 
     def is_pwd_encrypt(self):
-        print os.environ.get("CLOSE_PASSWORD_ENCRYPTION")
-        return 'close'
+        return os.environ.get("CLOSE_PASSWORD_ENCRYPTION")
 
     def setup(self, key): 
         self.bs = 32
