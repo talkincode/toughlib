4fdabd14c58dc1db97f71a0972c9fa93fdd07dea
toughman <wjt.net@outlook.com>
2016-04-21 12:07:27 +0800

    restore v1

--- toughlib/db_backup.py
+++ toughlib/db_backup.py
@@ -91,6 +91,18 @@ class DBBackup:
             'slc_rad_product_attr' : 'tr_product_attr',
         }
 
+        def filter_attr(objs):
+            for o in objs:
+                if 'member_id' in o:
+                    o['customer_id'] = o['member_id']
+                    del o['member_id']
+                elif 'member_name' in o:
+                    o['customer_name'] = o['member_name'] 
+                    del o['member_name']
+                elif 'member_desc' in o:
+                    o['customer_desc'] = o['member_desc']
+                    del o['member_desc']
+
         with self.dbengine.begin() as db:
             with gzip.open(restorefs,'rb') as rfs:
                 for line in rfs:
@@ -101,34 +113,16 @@ class DBBackup:
                         ctable = table_defines[obj['table']]
                         print "delete from %s"%ctable
                         db.execute("delete from %s"%ctable)
-                        print 'insert datas into %s'%ctable
+                        print self.metadata.tables[ctable].insert()
                         objs =  obj['data']
                         if len(objs) < 500:
-                            for o in objs:
-                                if 'member_id' in o:
-                                    o['customer_id'] = o['member_id']
-                                    del o['member_id']
-                                elif 'member_name' in o:
-                                    o['customer_name'] = o['member_name'] 
-                                    del o['member_name']
-                                elif 'member_desc' in o:
-                                    o['customer_desc'] = o['member_desc']
-                                    del o['member_desc']
+                            filter_attr(objs)
                             if objs:db.execute(self.metadata.tables[ctable].insert().values(objs))
                         else:
                             while len(objs) > 0:
                                 _tmp_pbjs = objs[:500]
                                 objs = objs[500:]
-                                for o in objs:
-                                    if 'member_id' in o:
-                                        o['customer_id'] = o['member_id']
-                                        del o['member_id']
-                                    elif 'member_name' in o:
-                                        o['customer_name'] = o['member_name']
-                                        del o['member_name']
-                                    elif 'member_desc' in o:
-                                        o['customer_desc'] = o['member_desc']
-                                        del o['member_desc']
+                                filter_attr(objs)
                                 db.execute(self.metadata.tables[ctable].insert().values(_tmp_pbjs))
                             
                         # db.execute("commit;")
