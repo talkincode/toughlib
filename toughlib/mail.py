679ad86f34f49e18e589c4bccbc50027130ec088
jamiesun <jamiesun.net@gmail.com>
2016-05-06 01:16:37 +0800

    send tls mail

--- toughlib/mail.py
+++ toughlib/mail.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+from cStringIO import StringIO
+from OpenSSL.SSL import SSLv3_METHOD
+from twisted.mail.smtp import ESMTPSenderFactory
+from twisted.internet.ssl import ClientContextFactory
+from twisted.internet.defer import Deferred
+from twisted.internet import reactor
 from toughlib import  utils
 from toughlib import dispatch,logger
 from twisted.mail.smtp import sendmail
@@ -11,40 +17,48 @@ import email
 class SendMail:
 
     def __init__(self, server='127.0.0.1', port=25, user=None, password=None,from_addr=''):
-        self.smtp_server = server
-        self.from_addr = from_addr
-        self.smtp_port = port
+        self.smtp_server = utils.safestr(server)
+        self.from_addr = utils.safestr(from_addr)
+        self.smtp_port = int(port)
         self.smtp_user = user
         self.smtp_pwd = password
 
-    def send_mail(self, mailto, topic, content, **kwargs):
+    def send_mail(self, mailto, topic, content, tls=False,**kwargs):
         message = email.MIMEText.MIMEText(content,'html', 'utf-8')
         message["Subject"] = email.Header.Header(topic,'utf-8')
         message["From"] = email.Header.Header("%s <%s>"%(self.from_addr[:self.from_addr.find('@')],self.from_addr),'utf-8')
         message["To"] = mailto
         message["Accept-Language"]="zh-CN"
         message["Accept-Charset"]="ISO-8859-1,utf-8"
-        if '@toughradius.org' in self.from_addr:
-            message['X-Mailgun-SFlag'] = 'yes'
-            message['X-Mailgun-SScore'] = 'yes'
-        return sendmail(self.smtp_server, self.from_addr, mailto, message,
+        if not tls:
+            logger.info('send mail')
+            return sendmail(self.smtp_server, self.from_addr, mailto, message,
                         port=self.smtp_port, username=self.smtp_user, password=self.smtp_pwd)
- 
+        else:
+            logger.info('send tls mail')
+            contextFactory = ClientContextFactory()
+            contextFactory.method = SSLv3_METHOD
+            resultDeferred = Deferred()
+            senderFactory = ESMTPSenderFactory(
+                self.smtp_user,
+                self.smtp_pwd,
+                self.from_addr,
+                mailto,
+                StringIO(message.as_string()),
+                resultDeferred,
+                contextFactory=contextFactory,
+                requireAuthentication=(self.smtp_user and self.smtp_pwd),
+                requireTransportSecurity=tls)
+
+            reactor.connectTCP(self.smtp_server, self.smtp_port, senderFactory)
+            return resultDeferred
+
 
 def send_mail(server='127.0.0.1', port=25, user=None, password=None, 
-                from_addr=None, mailto=None, topic=None, content=None, **kwargs):
+                from_addr=None, mailto=None, topic=None, content=None, tls=False, **kwargs):
     sender = SendMail(server,port,user,password,from_addr)
-    return sender.send_mail(mailto, topic, content, **kwargs)
+    return sender.send_mail(mailto, topic, content, tls=tls,**kwargs)
 
-
-if __name__ == '__main__':
-    from twisted.internet import reactor
-    def test():
-        d = send_mail(server='smtp.mailgun.org',port=25,user='service@toughradius.org',password='service2015',
-            from_addr='service@toughradius.org',mailto='6583805@qq.com',topic='this is a test mail',content="test mail")
-        d.addCallbacks(logger.info,logger.error)
-    reactor.callLater(0.1,test)
-    reactor.run()
 
 
 
