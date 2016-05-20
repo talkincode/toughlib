47ba07cb759ff3211eb544393f832a56fdb0cffb
jamiesun <jamiesun.net@gmail.com>
2016-05-20 15:01:59 +0800

    logger error

--- toughlib/logger.py
+++ toughlib/logger.py
@@ -145,7 +145,7 @@ class Logger:
         text = txlog.textFromEventDict(eventDict)
         if text is None:
             return
-        text = safeunicode(text)
+        text = repr(text)
         if eventDict['isError'] and 'failure' in eventDict:
             self.error(text)
         else:
