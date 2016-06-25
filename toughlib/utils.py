e9a7ad55d837f16ab2ad735ee35da28d3029a560
jamiesun <jamiesun.net@gmail.com>
2016-06-25 18:35:48 +0800

    add gb2kb func

--- toughlib/utils.py
+++ toughlib/utils.py
@@ -126,6 +126,16 @@ def kb2mb(ik,fmt='1.00'):
 def mb2kb(im=0):
     _mb = decimal.Decimal(im or 0)
     _kb = _mb * decimal.Decimal(1024)
+    return int(_kb.to_integral_value())    
+
+def kb2gb(ik,fmt='1.00'):
+    _kb = decimal.Decimal(ik or 0)
+    _mb = _kb / decimal.Decimal(1024*1024)
+    return str(_mb.quantize(decimal.Decimal(fmt)))
+    
+def gb2kb(im=0):
+    _mb = decimal.Decimal(im or 0)
+    _kb = _mb * decimal.Decimal(1024*1024)
     return int(_kb.to_integral_value())
     
 def hour2sec(hor=0):
