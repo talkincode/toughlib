930f68d42ade68c9b8786bd107bdd5569f0d1d26
jamiesun <jamiesun.net@gmail.com>
2016-06-27 16:00:45 +0800

    0.1.7 - 2016-06-27 16:00:45: no commit message

--- toughlib/btforms/__init__.py
+++ toughlib/btforms/__init__.py
@@ -85,7 +85,7 @@ class Form(object):
 
     def __init__(self, *inputs, **kw):
         self.inputs = inputs
-        self.attrs = Storage()
+        self.attrs = kwargs.pop('attrs',{})
         self.valid = True
         self.note = None
         self.validators = kw.pop('validators', [])
