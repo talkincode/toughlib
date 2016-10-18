0011707bbfe2feede988a2911b97e70236e19b4e
wangjuntao <wjt@toughstruct.com>
2016-10-18 23:36:19 +0800

    dbdump

--- toughlib/db_backup.py
+++ toughlib/db_backup.py
@@ -6,23 +6,24 @@ import json,os,gzip
 
 class DBBackup:
 
-    def __init__(self, sqla_metadata, excludes=[]):
+    def __init__(self, sqla_metadata, excludes=[],batchsize=49,**kwargs):
         self.metadata = sqla_metadata
         self.excludes = excludes
         self.dbengine = self.metadata.bind
+        self.batchsize = batchsize
 
     def dumpdb(self, dumpfile):
         _dir = os.path.split(dumpfile)[0]
         if not os.path.exists(_dir):
             os.makedirs(_dir)
 
-        with self.dbengine.begin() as db:
-            with gzip.open(dumpfile, 'wb') as dumpfs:
-                tables = {_name:_table for _name, _table in self.metadata.tables.items() if _name not in self.excludes}
-                table_headers = ('table_names', tables.keys())
-                dumpfs.write(json.dumps(table_headers, ensure_ascii=False).encode('utf-8'))
-                dumpfs.write('\n')
-                for _name,_table in tables.iteritems():
+        with gzip.open(dumpfile, 'wb') as dumpfs:
+            tables = {_name:_table for _name, _table in self.metadata.tables.items() if _name not in self.excludes}
+            table_headers = ('table_names', tables.keys())
+            dumpfs.write(json.dumps(table_headers, ensure_ascii=False).encode('utf-8'))
+            dumpfs.write('\n')
+            for _name,_table in tables.iteritems():
+                with self.dbengine.begin() as db:
                     rows = db.execute(select([_table]))
                     for row in rows:
                         obj = (_name, dict(row.items()))
@@ -36,11 +37,11 @@ class DBBackup:
             print 'backup file not exists'
             return
         
-        with self.dbengine.begin() as db:
-            with gzip.open(restorefs,'rb') as rfs:
-                cache_datas = {}
-                for line in rfs:
-                    try:
+        with gzip.open(restorefs,'rb') as rfs:
+            cache_datas = {}
+            for line in rfs:
+                try:
+                    with self.dbengine.begin() as db:
                         tabname, rdata = json.loads(line)
                         if tabname == 'table_names' and rdata:
                             for table_name in rdata:
@@ -48,30 +49,27 @@ class DBBackup:
                                 db.execute("delete from %s;" % table_name)
                             continue
 
-                        if tabname not in cache_datas:
-                            cache_datas[tabname] = [rdata]
-                        else:
-                            cache_datas[tabname].append(rdata)
+                        cache_datas.setdefault(tabname, []).append(rdata)
 
-                        if tabname in cache_datas and len(cache_datas[tabname]) >= 49:
-                            print 'insert datas<%s> into %s' % (len(cache_datas[tabname]), tabname)
+                        if len(cache_datas[tabname]) >= self.batchsize:
+                            print 'batch insert datas<%s> into %s' % (len(cache_datas[tabname]), tabname)
                             db.execute(self.metadata.tables[tabname].insert().values(cache_datas[tabname]))
                             del cache_datas[tabname]
+                except:
+                    print 'error data %s ...'% line
+                    raise
 
-                    except:
-                        print 'error data %s ...'% line
-                        raise
-
-                print "insert last data"
-                for tname, tdata in cache_datas.iteritems():
-                    try:
-                        print 'insert datas<%s> into %s' % (len(tdata), tname)
+            print "insert last data"
+            for tname, tdata in cache_datas.iteritems():
+                try:
+                    print 'batch insert datas<%s> into %s' % (len(tdata), tname)
+                    with self.dbengine.begin() as db:
                         db.execute(self.metadata.tables[tname].insert().values(tdata))
-                    except:
-                        print 'error data %s ...' % tdata
-                        raise
+                except:
+                    print 'error data %s ...' % tdata
+                    raise
 
-                cache_datas.clear()
+            cache_datas.clear()
 
     def restoredbv1(self,restorefs):
         if not os.path.exists(restorefs):
